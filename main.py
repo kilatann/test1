beb8deafc40996851e96b59b6b8fc0961fbb1f1f
IsaXl Local <yipsim2@gmail.com>
2023-11-24 16:04:37 +0100

    prenier commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+
+
+
+pygame.init()
+background = pygame.image.load("asset/bg.jpg")
+
+pygame.display.set_caption("plant_vs_zombie")
+pygame.display.set_mode((1024, 640))
+
+running = True
+
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = false
+
+
+
+
